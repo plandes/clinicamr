87a8ae2a5ea8630666ae040c5ce5d0a3f1cbd870
Paul Landes <landes@mailc.net>
2022-07-01 10:28:00 -0500

    CLI admission plot generation from CLI and streamlined config

--- src/python/zensols/clinicamr/app.py
+++ src/python/zensols/clinicamr/app.py
@@ -6,10 +6,11 @@ __author__ = 'Paul Landes'
 from dataclasses import dataclass, field
 import logging
 from pathlib import Path
+import pandas as pd
 from zensols.config import ConfigFactory
 from zensols.persist import Stash
 from zensols.nlp import FeatureToken, FeatureDocumentParser
-from zensols.mimic import Corpus, HospitalAdmission
+from zensols.mimic import Corpus, Note, HospitalAdmission
 from zensols.mimicsid import AnnotationResource
 
 logger = logging.getLogger(__name__)
@@ -20,9 +21,7 @@ class Application(object):
     """Clincial Domain Abstract Meaning Representation Graphs.
 
     """
-    CLI_META = {
-        'option_excludes':
-        set('config_factory doc_parser corpus anon_resource'.split())}
+    CLI_META = {'option_includes': set('text'.split())}
 
     config_factory: ConfigFactory = field()
     """For prototyping."""
@@ -34,9 +33,13 @@ class Application(object):
     """A container class for the resources that access the MIMIC-III corpus."""
 
     anon_resource: AnnotationResource = field()
-    """Contains resources to acces the MIMIC-III MedSecId annotations."""
+    """Contains resources to access the MIMIC-III MedSecId annotations."""
 
     plot_path: Path = field()
+    """Base path to add AMR graphs."""
+
+    amr_paragraph_stash: Stash = field()
+    """The stash that caches paragraph AMR graphs."""
 
     def __post_init__(self):
         FeatureToken.WRITABLE_FEATURE_IDS = tuple('norm cui_'.split())
@@ -55,7 +58,12 @@ class Application(object):
             print(doc.amr)
             doc.amr.plot(Path('/d/amr'))
 
-    def proto(self):
+    def clear_cache(self):
+        """Clear the paragraph AMR cache."""
+        logger.info(f'removing files in: {self.amr_paragraph_stash.path}')
+        self.amr_paragraph_stash.clear()
+
+    def _tmp1(self):
         """Used for rapid prototyping."""
         hadm_id = '119960'
         stash: Stash = self.corpus.hospital_adm_stash
@@ -65,16 +73,26 @@ class Application(object):
         for para in sec.paragraphs:
             para.amr.plot(self.plot_path / f'{adm.hadm_id}-{note.row_id}')
 
-    def protoX(self):
-        hadm_id = '119960'
+    def plot(self, hadm_id: int = '119960'):
+        """Create plots for an admission."""
+        parser_model: str = self.config_factory.config.get_option(
+            'parse_model', section='amr_default')
+        df: pd.DataFrame = self.anon_resource.note_ids
         stash: Stash = self.corpus.hospital_adm_stash
         adm: HospitalAdmission = stash[hadm_id]
-        note = adm.notes_by_category['Discharge summary'][0]
-        sec = note.sections['history-of-present-illness']
-        doc = sec.body_doc
-        doc = doc.combine_sentences()
-        print(doc.text)
-        ann = self.config_factory('amr_annotator')
-        amr_doc = ann(doc)
-        print(amr_doc.amr.graph_string)
-        amr_doc.amr.plot()
+        anon_row_ids = df[df['hadm_id'] == hadm_id]['row_id'].astype(int).tolist()
+        note: Note
+        for note in map(lambda i: adm.notes_by_id[i], anon_row_ids):
+            id_dir: str = f'{adm.hadm_id}-{note.row_id}'
+            note_path: Path = self.plot_path / parser_model / id_dir
+            print(note)
+            for sec in note.sections.values():
+                try:
+                    for para in sec.paragraphs:
+                        para.amr.plot(note_path)
+                except Exception as e:
+                    print(f'Error: {e}')
+
+    def proto(self):
+        """Used for rapid prototyping."""
+        self.plot()
