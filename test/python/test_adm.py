f16f6b7a3cb9d322a01bd49abe8306f6eda861c6
Paul Landes <landes@mailc.net>
2024-04-18 19:02:29 -0500

    add unit testing

--- test/python/test_adm.py
+++ test/python/test_adm.py
@@ -13,25 +13,51 @@ class TestAdmissionGraph(TestBase):
     it makes.
 
     """
-    def test_adm_graph(self):
-        if self._validate_db_exists():
-            self._test_adm_graph()
+    def setUp(self):
+        super().setUp()
+        self.should_file = Path('test-resources/should-graph.txt')
 
-    def _test_adm_graph(self):
-        WRITE: bool = 0
-        should_file = Path('test-resources/should-graph.txt')
+    def test_adm_graph_create(self):
+        if self._validate_db_exists():
+            self._test_adm_graph_create()
+
+    def test_pickle(self):
+        if self._validate_db_exists():
+            self._test_pickle()
+
+    def _get_adm(self) -> AdmissionAmrFeatureDocument:
         stash: AdmissionAmrFactoryStash = self.config_factory(
             'camr_adm_amr_factory_stash')
         self.assertTrue(isinstance(stash, AdmissionAmrFactoryStash))
         #hadm_id: str = '134891'  # human annotated
         hadm_id: str = '151608'  # model annotated
-        adm: AdmissionAmrFeatureDocument = stash.load(hadm_id)
+        return stash.load(hadm_id)
+
+    def _get_should(self) -> str:
+        with open(self.should_file) as f:
+            return f.read()
+
+    def _test_adm_graph_create(self):
+        WRITE: bool = 0
+        adm: AdmissionAmrFeatureDocument = self._get_adm()
         if WRITE:
-            with open(should_file, 'w') as f:
+            with open(self.should_file, 'w') as f:
                 adm.write(writer=f)
-        with open(should_file) as f:
-            should: str = f.read()
+        should: str = self._get_should()
         sio = StringIO()
         adm.write(writer=sio)
+        actual: str = sio.getvalue()
+        self.assertEqual(should, actual)
+
+    def _test_pickle(self):
+        adm: AdmissionAmrFeatureDocument = self._get_adm()
+        should: str = self._get_should()
+
+        adm2 = self._pickle(adm)
+        self.assertNotEqual(id(adm), id(adm2))
+        self.assertEqual(adm, adm2)
+
+        sio = StringIO()
+        adm2.write(writer=sio)
         actual: str = sio.getvalue()
         self.assertEqual(should, actual)
--- test/python/util.py
+++ test/python/util.py
@@ -2,17 +2,22 @@ import unittest
 import os
 import sys
 from pathlib import Path
+from io import BytesIO
+import pickle
 import shutil
+import logging
 from zensols.db.sqlite import SqliteConnectionManager
 from zensols.persist import persisted
 from zensols.clinicamr import ApplicationFactory
 from zensols.mednlp import surpress_warnings
 
+logger = logging.getLogger(__name__)
+
 
 class TestBase(unittest.TestCase):
     def __init__(self, *args, **kwargs):
-        self.maxDiff = sys.maxsize
         super().__init__(*args, **kwargs)
+        self.maxDiff = sys.maxsize
         surpress_warnings()
         os.environ.pop('CLINICAMRRC', None)
         self._clear_cache()
@@ -25,15 +30,6 @@ class TestBase(unittest.TestCase):
         return harness.get_config_factory(
             '-c test-resources/test.conf --level err')
 
-    def _validate_db_exists(self) -> bool:
-        self._config_logging()
-        mng: SqliteConnectionManager = \
-            self.config_factory('mimic_sqlite_conn_manager')
-        if not mng.db_file.exists():
-            print('no MIMIC-III database to test with--skipping')
-            return False
-        return True
-
     @property
     def doc_parser(self) -> 'FeatureDocumentParser':
         config_factory = self.config_factory
@@ -41,11 +37,25 @@ class TestBase(unittest.TestCase):
             'doc_parser', 'clinicamr_default')
         return config_factory(sec)
 
+    def _validate_db_exists(self) -> bool:
+        self._config_logging()
+        mng: SqliteConnectionManager = \
+            self.config_factory('mimic_sqlite_conn_manager')
+        if not mng.db_file.exists():
+            logger.error('no MIMIC-III database to test with--skipping')
+            return False
+        return True
+
     def _clear_cache(self):
         targ_dir = Path('target')
         if targ_dir.is_dir():
             shutil.rmtree(targ_dir)
 
     def _config_logging(self):
-        import logging
         logging.basicConfig(level=logging.ERROR)
+
+    def _pickle(self, obj):
+        bio = BytesIO()
+        pickle.dump(obj, bio)
+        bio.seek(0)
+        return pickle.load(bio)
